f4b37f2830868048af2e24b5eba0710bd45ace14
spensation <spencevans@gmail.com>
2020-02-08 23:59:10 -0500

    done!

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+	if array.size > 2
+		new_array = array[0..-2]
+		str = ""
+		new_array.each do |word|
+			str << word + "," + " "
+		end
 
+		str[0..-2] + " " + "and" + " " + array[-1]
+	elsif array.size > 1
+		array[0] + " " + "and" + " " + array[1]
+	else
+		array[0]
+	end
 end
